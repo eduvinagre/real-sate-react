32a167114d3f4a03f00048d5f88dd3fd357d0eaa
eduvinagre <eduvinagre@gmail.com>
2023-04-19 13:00:12 -0300

    header js and CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 import "./Header.css"
 export const Header = () => {
   return (
-<section className="w-wrapper">
+<section className="h-wrapper">
     <div className="flexCenter paddings innerWidth">
 
         <img src="./logo.png" alt="logo" width={100} />
 
         <div className="h-menu">
-            <a href="">
-                Residencias
-            </a>
-                <a href="">Nossos Valores</a>
-                <a href="">Entre em Contato</a>
-                <a href="">Comece Aqui</a>
-                <button className='button'>
-                <a href="">Contato</a>
-                </button>
-                    
+            <a href="">Residencias</a>
+            <a href="">Nossos Valores</a>
+            <a href="">Entre em Contato</a>
+            <a href="">Comece Aqui</a>
+            <button className='button'>
+            <a href="">Contato</a>
+            </button>
         </div>
     </div>
 </section>
