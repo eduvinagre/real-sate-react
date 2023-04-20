2ee2251e13ed93963fce6c87275806684461b9ee
eduvinagre <eduvinagre@gmail.com>
2023-04-20 17:57:16 -0300

    residencies jsx and css update

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import "./Residencies.css";
-
+import data from "../../utils/slider.json";
 const Residencies = () => {
   return <div>Residencies</div>;
 };
