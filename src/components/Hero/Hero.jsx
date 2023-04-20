9cea367db5865b03a99592ad824daf62d6185fd6
eduvinagre <eduvinagre@gmail.com>
2023-04-19 23:38:55 -0300

    hero jsx and css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,23 @@ export const Hero = () => {
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
         {/* left side */}
-        <div className="hero-left"></div>
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <h1>
+              Descubra
+              <br /> o Imóvel Mais
+              <br /> Adequado
+            </h1>
+          </div>
+          <div className="flexColStart hero-description">
+            <span>
+              Descubra uma variedade de imóveis que combinam com o seu estilo
+            </span>
+            <span>Esqueça todas as dificuldades para encontrar o seu lar</span>
+          </div>
+
+          <div className="search-bar">Search Bar</div>
+        </div>
         {/* right side */}
         <div className="flexCenter hero-right">
           <div className="image-container">
