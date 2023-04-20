7831db44174818908787362a4a65576f5626410d
eduvinagre <eduvinagre@gmail.com>
2023-04-20 17:42:34 -0300

    companies jsx and css

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Companies.css";
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="logo da prologis" />
+        <img src="./tower.png" alt="logo da tower" />
+        <img src="./equinix.png" alt="logo da equinix" />
+        <img src="./realty.png" alt="logo da realty " />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
