c3f450922137addf1a78ae3a6975707d76593838
eduvinagre <eduvinagre@gmail.com>
2023-04-21 13:19:30 -0300

    first commit of Values section

---
+++ src/components/Values/Value.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Value.css";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import {MdOutlineArrowDropDown} from 'react-icons/md'
+function Value() {
+  return (
+
+  )
+}
+
+export default Value;
