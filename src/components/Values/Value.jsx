946e337b2a60b629fccf8ff840825f422174c7cd
eduvinagre <eduvinagre@gmail.com>
2023-04-21 20:05:35 -0300

    value jsx adjusment

--- src/components/Values/Value.jsx
+++ src/components/Values/Value.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./Value.css";
 import {
   Accordion,
   AccordionItem,
@@ -10,6 +9,7 @@ import {
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from "react-icons/md";
+import "./Value.css";
 import data from "../../utils/accordion";
 function Value() {
   return (
