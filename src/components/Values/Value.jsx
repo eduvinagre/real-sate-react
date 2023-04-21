1a435a96ecbcf52e4ee303b151dd2c29a78d437e
eduvinagre <eduvinagre@gmail.com>
2023-04-21 13:57:58 -0300

    setting accordion first jsx and css

--- src/components/Values/Value.jsx
+++ src/components/Values/Value.jsx
@@ -9,11 +9,30 @@ import {
   AccordionItemState,
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
-import {MdOutlineArrowDropDown} from 'react-icons/md'
+import { MdOutlineArrowDropDown } from "react-icons/md";
 function Value() {
   return (
-
-  )
+    <section className="v-wrapper">
+      <div className="paddings innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="casa" />
+          </div>
+        </div>
+        {/* right side */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Nossos Valores</span>
+          <span className="primaryText">Nossos Valores para Você</span>
+          <span className="secondaryText">
+            Estamos sempre prontos para proporcionar os melhores serviços a
+            você. <br />
+            Nós acreditamos que um bom lugar para morar torna a sua vida melhor.
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Value;
