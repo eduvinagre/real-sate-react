6e57658a64bb45cac1b4948897b6e5ce2e6d81ba
eduvinagre <eduvinagre@gmail.com>
2023-04-22 15:36:46 -0300

    contact jsx css jpg

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Contact.css";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
+
 const Contact = () => {
   return (
     <section className="c-wrapper">
@@ -12,6 +16,38 @@ const Contact = () => {
             Estamos sempre preparados para oferecer o melhor serviço para você.
             Acreditamos que um bom lugar para morar torna a vida melhor.
           </span>
+
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexColStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Ligue</span>
+                    <span className="secondaryText">(99) 9999-9999</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Ligue Agora</div>
+              </div>
+
+              {/* second mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText">(99) 9999-9999</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Chat Agora</div>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* right side */}
