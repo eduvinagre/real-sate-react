4712d1e13c3f729de6f6813b38104a31299d06f4
eduvinagre <eduvinagre@gmail.com>
2023-04-22 17:29:52 -0300

    footer jsx and css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,21 @@ const Footer = () => {
           <img src="./logo2.png" alt="logo da homyz" width={120} />
 
           <span className="secondaryText">
-            Nossa visão é dar a todos <br />o melhor lugar para viver.
+            Nosso objetivo é dar a todos <br />o melhor lugar para viver.
           </span>
+        </div>
+        {/* right side */}
+        <div className="flexColStart f-right">
+          <span className="primaryText">Informação</span>
+          <span className="secondaryText">
+            Rua Kobe 2704, Belo Horizonte, MG
+          </span>
+          <div className="flexCenter f-menu">
+            <span>Propriedade</span>
+            <span>Serviços</span>
+            <span>Produtos</span>
+            <span>Sobre nós</span>
+          </div>
         </div>
       </div>
     </section>
