9bcd02365a8a6b7b52dae5ec58c65fa934bdde24
eduvinagre <eduvinagre@gmail.com>
2023-04-23 16:50:50 -0300

    responsive menu functions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { BiMenuAltRight } from "react-icons/bi";
 export const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const getMenuStyles = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpened && "-100%" };
+    }
+  };
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
 
-        <div className="flexCenter h-menu">
+        <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
           <a href="">Residências</a>
           <a href="">Nossos Valores</a>
           <a href="">Entre em Contato</a>
@@ -16,7 +23,10 @@ export const Header = () => {
             <a href="">Contato</a>
           </button>
         </div>
-        <div className="menu-icon">
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
           <BiMenuAltRight size={30} />
         </div>
       </div>
